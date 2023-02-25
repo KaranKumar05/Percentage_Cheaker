5b67c7d93d0992d0f0235e95dbb8130ee615c91b
KARAN KUMAR <kkvmalhi@gmail.com>
2023-02-25 10:33:27 +0500

    javaScript Updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let value = prompt("Enter value: ");
-if (value < 0) {
-  alert("Please Enter Valid value");
+let value = prompt("Enter your Percentage: ");
+if (value < 0 || value>100 ) {
+  alert("Please Enter Valid Percentage Between 0-100");
 } else if (value < 50) {
   alert("Your are Fail");
 } else if (value < 60) {
@@ -11,6 +11,6 @@ if (value < 0) {
   alert("Your Grade is A");
 } else if (value <= 100) {
   alert("Your Grade is A+");
-} else if (value > 100) {
-  alert("Please Enter Valid value");
-}
+} else[
+  alert("Can't Find Any Record")
+]
